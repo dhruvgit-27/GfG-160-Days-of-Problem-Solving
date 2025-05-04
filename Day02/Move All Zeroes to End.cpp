2749b321a1746594d5a66ceaf65291ac81fe8400
Dhruv Pratap Singh <thakurdhruv2701@gmail.com>
2025-05-04 11:43:06 +0530

    ✨ Add Day 02 solution

---
+++ Day02/Move All Zeroes to End.cpp
@@ -0,0 +1,19 @@
+
+// User function template for C++
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        int n = arr.size();
+        int nonZeroIndex = 0;
+        
+        for(int i = 0; i < n; i++){
+            if (arr[i] !=0) {
+                arr[nonZeroIndex++] = arr[i];
+            }
+        }
+        
+        while (nonZeroIndex < n) {
+            arr[nonZeroIndex++] = 0;
+        }
+    }
+};
