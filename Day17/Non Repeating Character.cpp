95da41301542b44b85115e7ae961d54a6420456b
Dhruv Pratap Singh <thakurdhruv2701@gmail.com>
2025-05-19 22:39:27 +0530

    Day 17: Non Repeating Character in  Cpp

---
+++ Day17/Non Repeating Character.cpp
@@ -0,0 +1,19 @@
+
+
+
+
+class Solution {
+  public:
+    char nonRepeatingChar(string &s) {
+        int freq[26] = {0};
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+        
+        for (char ch : s) {
+            if (freq[ch - 'a'] == 1)
+            return ch;
+        }
+        return '$';
+    }
+};
