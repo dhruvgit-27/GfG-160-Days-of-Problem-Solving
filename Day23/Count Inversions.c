a1538ebd1ac8c1b5ee0728b66dec778cdc42664f
Dhruv Pratap Singh <thakurdhruv2701@gmail.com>
2025-05-25 22:00:15 +0530

    Day 23: Count Inversions in  C

---
+++ Day23/Count Inversions.c
@@ -0,0 +1,43 @@
+
+
+
+long long merge(int arr[], int temp[], int left, int mid, int right) {
+    int i = left;
+    int j = mid;
+    int k = left;
+    long long inv_count = 0;
+    
+    while (i <= mid - 1 && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+            inv_count += (mid - i);
+        }
+    }
+    while (i <= mid - 1)
+        temp[k++] = arr[i++];
+    while (j <= right)
+        temp[k++] = arr[j++];
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+    
+    return inv_count;
+}
+
+long long mergeSort(int arr[], int temp[], int left, int right) {
+    long long inv_count = 0;
+    if (left < right) {
+        int mid = (left + right) / 2;
+        inv_count += mergeSort(arr, temp, left, mid);
+        inv_count += mergeSort(arr, temp, mid + 1, right);
+        inv_count += merge(arr, temp, left, mid + 1, right);
+    }
+    return inv_count;
+}
+
+int countInversions(int arr[], int n) {
+    int temp[n];
+    return mergeSort(arr, temp, 0, n - 1);
+    
+}
